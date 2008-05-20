7f86f6feb3db5d32de641ca9fd46ffff64b93f72
Mike Kinney <mike@pointlesscrap.net>
2008-05-20 18:56:54 +0000

    Changed license to Creative Commons.  Updated version to 0.7.6
    git-svn-id: svn://projects.pointlesscrap.net/mkabefook/trunk@30 eb3d8902-d5b4-dc11-9781-006097d2db73

--- MKAbeFook.h
+++ MKAbeFook.h
@@ -3,7 +3,7 @@
  *  MKAbeFook
  *
  *  Created by Mike on 10/18/06.
- *  Copyright 2006 Mike Kinney. All rights reserved.
+ *  
  *
  */
 #import "MKFacebook.h"
--- NSXMLElementAdditions.m
+++ NSXMLElementAdditions.m
@@ -1,31 +1,14 @@
 /*
- Copyright (c) 2008, Mike Kinney
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
- following conditions are met:
- 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the 
- following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
- following disclaimer in the documentation and/or other materials provided with the distribution.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
- USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
- */
 
-//
-//  NSXMLElementAdditions.m
-//  BRAP Client
-//
-//  Created by Mike Kinney on 11/27/07.
-//  Copyright 2008 Mike Kinney. All rights reserved.
-//
+ NSXMLElementAdditions.m
+ BRAP Client
+
+ Created by Mike Kinney on 11/27/07.
+
+ This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License.
+ To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/us/legalcode or send a letter to Creative Commons, 543 Howard Street, 5th Floor, San Francisco, California, 94105, USA.
+ 
+*/
 
 #import "NSXMLElementAdditions.h"
 
